57284b1085d1042f534ee346f6d06bb2617717a9
Will Johansson <will.johansson@rd.io>
2015-06-24 13:26:28 -0700

    Initial Commit

---
+++ AFNetworkingTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AFNetworkingTest
+//
+//  Created by Will Johansson on 2015-06-24.
+//  Copyright © 2015 Johansson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
